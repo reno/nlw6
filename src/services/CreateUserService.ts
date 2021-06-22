5d02bc6f4567fcae7279de8775cb270c152f4ac8
reno <renan.modenese@gmail.com>
2021-06-22 18:47:43 -0300

    Incluindo service

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from 'typeorm';
+import { UsersRepositories } from '../repositories/UsersRepositories';
+
+interface IUserRequest {
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+
+class CreateUserService {
+  async execute({ name, email, admin }: IUserRequest) {
+    const usersRepository = getCustomRepository(UsersRepositories);
+
+    console.log('user', name, email, admin);
+
+    if (!email) {
+      console.log('Email vazio')
+      //throw new Error('Email incorrect');
+    }
+
+    const userAlreadyExists = await usersRepository.findOne({ email });
+
+    if (userAlreadyExists) {
+      console.log('Email ja utilizado');
+      //throw new Error('User already exists');
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      admin,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService };
