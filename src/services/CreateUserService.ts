d2752f97d64d8e074a783c4f06f0ee75085e89a4
reno <renan.modenese@gmail.com>
2021-06-22 23:16:19 -0300

    Refatorando

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -8,29 +8,22 @@ interface IUserRequest {
 }
 
 class CreateUserService {
+
   async execute({ name, email, admin }: IUserRequest) {
     const usersRepository = getCustomRepository(UsersRepositories);
-
     if (!email) {
-      console.log('Email vazio')
       throw new Error('Email incorrect');
     }
-
     const userAlreadyExists = await usersRepository.findOne({ email });
-
     if (userAlreadyExists) {
-      console.log('Email ja utilizado');
       throw new Error('User already exists');
     }
-
     const user = usersRepository.create({
       name,
       email,
       admin,
     });
-
     await usersRepository.save(user);
-
     return user;
   }
 }
