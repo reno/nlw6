fa7b5927953bd97deb24037fb4125628c4d03df8
reno <renan.modenese@gmail.com>
2021-06-22 23:15:58 -0300

    Incluindo tratamento de erros

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,31 @@
 import 'reflect-metadata';
-import express from 'express';
-import { router } from "./routes";
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+
+import { router } from './routes';
 import './database';
 
 const app = express();
-const port = 3000;
 
 app.use(express.json());
 
 app.use(router);
 
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof Error) {
+      return response.status(400).json({
+        error: err.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  }
+);
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`)
-})
+});
