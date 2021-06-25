21eec3c5dab1636307863b79f15eb33ab4dcb0b9
reno <renan.modenese@gmail.com>
2021-06-24 22:33:00 -0300

    Atualizando middleware

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,17 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
+import { UsersRepositories } from "../repositories/UsersRepositories";
+import { getCustomRepository } from "typeorm";
 
-export function ensureAdmin(
+export async function ensureAdmin(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
-  // Verificar se usuario admin
-  const admin = true;
-
+  const { user_id } = request;
+  const usersRepositories = getCustomRepository(UsersRepositories);
+  const { admin } = await usersRepositories.findOne(user_id);
   if (admin) {
     return next();
   }
-
   return response.status(401).json({
     error: 'Unauthorized',
   });
